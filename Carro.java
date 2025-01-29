bb74c0ba6983ba30123752f81e2c73ccfe3ebbd8
Cleiton <cleitonkeliton.s@gmail.com>
2025-01-29 14:43:52 -0300

    Corrigir método setModelo na classe Carro

--- Carro.java
+++ Carro.java
@@ -17,7 +17,7 @@ public class Carro {
     public void setMarca (String marca) {this.marca=marca;}
 
     public String getModelo(){return modelo;}
-    public void setModelo (String modelo){ this.modelo=marca;}
+    public void setModelo (String modelo){ this.modelo=modelo;}
 
     public int getAno(){return ano;}
     public void setAno(int ano){this.ano=ano;}
